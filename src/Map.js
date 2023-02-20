bdbf4927d7d72271ae5bd509c9b079463b8801ec
Mame Coumba Ka <mck@Mames-MBP-2.attlocal.net>
2023-02-19 21:03:33 -0800

    geofiles

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 
-const geoUrl ="map.geojson"
+const geoUrl ="https://raw.githubusercontent.com/coumbaK/castrovisualstory/master/src/map.geojson?token=GHSAT0AAAAAAB54OFY34SJZDCGNABIFBPQ2Y7TBSOA"
   
   function MapChart() {
     return (
